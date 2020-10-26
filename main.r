b990b38d0ff1f75052ab4203b081936d15fe4cc1
Stanislaw Czembor <stanislawczembor@MacBook-Air-Stanislaw.local>
2020-10-26 19:58:30 +0100

    improved matrix multiplication

--- main.r
+++ main.r
@@ -8,8 +8,6 @@ setwd(working_dir)
 
 #loading file names
 filenames <- list.files()
-parse_number(filenames[1])
-
 
 #loading each image to one row in matrix A
 img <- readImage(filenames[1])
@@ -18,7 +16,6 @@ green <- as.vector(t(img[,,2]))
 blue <- as.vector(t(img[,,3]))
 A <- matrix(c(red,green,blue),nrow = 1)
 B <- matrix(c(parse_number(filenames[1])),nrow = 1)
-B
 for(i in 2:length(filenames))
 {
   img <- readImage(filenames[i])
@@ -29,18 +26,25 @@ for(i in 2:length(filenames))
   B <- rbind(B, c(parse_number(filenames[i])))
 }
 #concatenating image data (A), with its ID (B)
-A <- cbind(A,B)
-nrow(A)
+#A <- cbind(A,B)
+#nrow(A)
 
-macierz_jednostkowa <- matrix(1.0, nrow = nrow(A), ncol = 1)
-dim(macierz_jednostkowa)
-typeof(macierz_jednostkowa)
-macierz_srednie <- matrix(colMeans(A),nrow = 1)
-dim(macierz_srednie)
-typeof(macierz_srednie)
-X_mean <- macierz_jednostkowa %*% macierz_srednie
+vector_ones <- matrix(1.0, nrow = nrow(A), ncol = 1)
+vector_ones
+dim(vector_ones)
+typeof(vector_ones)
+col_means <- matrix(colMeans(A),nrow = 1)
+dim(col_means)
+typeof(col_means)
+X_mean <- vector_ones %*% col_means
 X_mean[1:20,1:20]
 dim(matrix(colMeans(A),nrow = 1))
+#substracting the mean
+B = A-X_mean
+dim(B)
+#computing the cov matrix
+Sigma = t(B)%*%B
+cov(B)
 #TODO podział zestawu danych na treningowy i validacyjny (każda klasa tj. każde zdj musi być równie liczna)
 
 
